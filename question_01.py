5aeb4636e71c9eb19d4a1f03e9c685255218b8ed
shway <shuwei4@ualberta.ca>
2022-11-27 16:08:08 -0700

    first prototye done

--- question_01.py
+++ question_01.py
@@ -4,6 +4,26 @@
 
 ## Imports
 import numpy as np
+from random import uniform
+
+# initialize the input sequence
+def init_input_seq(p_min, p_max, epsilon, n):
+	seq = []
+	for i in range(n):
+		# exclude 0 when randomly initializing w
+		w = uniform(0, epsilon)
+		while (w == 0):
+			w = uniform(0, epsilon)
+			
+		# initialize v randomly
+		ratio = uniform(p_min, p_max)
+		v = w * ratio
+		
+		# append v and w to seq
+		assert(v/w >= p_min and v/w <= p_max)
+		seq.extend([v, w])
+	return seq
+
 
 # the threshold function from pp. 25 of lec. 7
 def threshold(p_min, p_max, y):
@@ -13,27 +33,46 @@ def threshold(p_min, p_max, y):
 	elif (y >= beta and y <= 1):
 		return p_min * np.exp(y / beta - 1)
 
+
 # the threshold-based algorithm from pp. 17 of lec. 7
-def threshold_based_alg(p_min, p_max, T):
-	for t in range(T):
+def threshold_based_alg(p_min, p_max, input_seq):
+	y = 0
+	decision_seq = []
+	for t in range(0, len(input_seq), 2):
+		v = input_seq[t]
+		w = input_seq[t + 1]
+		p = threshold(p_min, p_max, y)
 		if (v/w < p):
-			x_t = 0
-		else:
-			x_t = 1
-		y_t = 
+			x = 0
+		elif (v/w >= p):
+			x = 1
+		y += w * x
+		decision_seq.append(x)
+	return decision_seq
+
 
 if (__name__ == "__main__"):
 	# the lower bound on threshold
-	p_min = 0
+	p_min = 0.1
 	
 	# the upper bound on threshold
 	p_max = 2
 	
+	# the epsilon value
+	epsilon = 0.001
+	
 	# number of iterations
-	T = 100
+	n = 100
+	
+	# initialize the input sequence
+	input_seq = init_input_seq(p_min, p_max, epsilon, n)
+	
+	print(input_seq)
 	
 	# compute the answer by the threshold-based algorithm
-	ans = threshold_based_alg(p_min, p_max, T)
+	ans = threshold_based_alg(p_min, p_max, input_seq)
+	
+	print(ans)
 	
 	
 	
